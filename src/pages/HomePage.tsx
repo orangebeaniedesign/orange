090e44965b28747c867cba44b44a00b202283269
orangebeaniedesign <c4982tqjkw@privaterelay.appleid.com>
2026-02-09 14:44:20 +0000

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -44,7 +44,7 @@ function HeroSection() {
         className="absolute inset-0"
       >
         <img
-          src="/dscf2906.jpg"
+          src="/public/dscf2906.jpg"
           alt="Hero background"
           className="w-full h-full object-cover"
         />
