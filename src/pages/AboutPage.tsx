ba5806f22334355fd80183956abf4f77fd8426b2
orangebeaniedesign <c4982tqjkw@privaterelay.appleid.com>
2026-02-09 14:59:58 +0000

    Updated AboutPage.tsx

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -130,7 +130,8 @@ function StatementSection() {
               transition={{ duration: duration.slowest, ease: easing.expoOut }}
               className="font-serif text-display-xl leading-[1.2] mb-14"
             >
-              Design, for me, is just that, a way to turn ideas into moments that leave a mark.
+              Design, for me, is just that, 
+              a way to turn ideas into moments that leave a mark.
             </motion.p>
 
             <motion.div
